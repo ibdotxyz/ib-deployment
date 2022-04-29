52b79769ac12638dcefdfc5d764df98237f35bb8
IronBankDev <dev@ib.xyz>
2022-04-29 12:44:43 +0800

    update fantom deployments

--- deploy/004_deploy_price_oracle_proxy.ts
+++ deploy/004_deploy_price_oracle_proxy.ts
@@ -8,16 +8,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployer, nativeUsdAggregator, admin, guardian} = await getNamedAccounts();
   const priceOracleV1Address = (await get('PriceOracleV1')).address
 
-  await deploy('PriceOracleProxyUSD', {
+  await deploy('PriceOracleProxyFTM', {
     from: deployer,
-    args: [deployer, priceOracleV1Address, nativeUsdAggregator],
+    args: [deployer, priceOracleV1Address, '0xDA7a001b254CD22e46d3eAB04d937489c93174C3'],
     log: true,
   });
 
-  await execute('PriceOracleProxyUSD', {from: deployer}, '_setGuardian', guardian);
+  await execute('PriceOracleProxyFTM', {from: deployer}, '_setGuardian', guardian);
   // await execute('PriceOracleProxyUSD', {from: deployer}, '_setAdmin', admin);
 
 };
 export default func;
 func.tags = ['PriceOracleProxyUSD'];
-func.dependencies = ['PriceOracleV1'];
