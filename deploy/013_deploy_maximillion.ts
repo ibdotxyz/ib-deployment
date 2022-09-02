d373111ce2a1a08f56f643393ecf054e1f511b2a
satibxyz <101393953+satibxyz@users.noreply.github.com>
2022-09-02 16:01:43 +0800

    deploy: Maximillion.sol

---
+++ deploy/013_deploy_maximillion.ts
@@ -0,0 +1,20 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy, get} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  const cWrappedNative = await get('iWETH')
+
+  await deploy('Maximillion', {
+    from: deployer,
+    args: [cWrappedNative.address],
+    log: true,
+  });
+
+};
+export default func;
+func.tags = ['Maximillion'];
